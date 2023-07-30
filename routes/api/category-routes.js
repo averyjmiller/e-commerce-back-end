e6f17dca8f377c373ad0ba1187ad97b44221f73d
Avery Miller <averymiller2060@gmail.com>
2023-07-29 22:35:23 -0400

    implemented the DELETE route for the /api/categories endpoint

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,6 +8,7 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
     });
+    
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -47,18 +48,35 @@ router.put('/:id', async (req, res) => {
         id: req.params.id,
       },
     });
+
     if(!categoryData[0]) {
       res.status(404).json({ message: `No category found with id of ${req.params.id}` });
       return;
     }
+
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: `No category found with id of ${req.params.id}` });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
